4c0ba49bbd57cc646a99b75864db7c2c5a602e43
sotero.developer <sotero.developer@gmail.com>
2025-05-18 20:23:47 +0000

    adicona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # desenvolvimentoFullStack
+
 Desenvolvimento para implementação tecnologica atraves do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-return <h1>Milca, bom dia! Você precisa desbloquear o silenciador do whatsApp para que eu possa visualizar seus status. 
-</h1>
+function Home() {
+  return (
+    <h1>
+      Milca, bom dia! Você precisa desbloquear o silenciador do whatsApp para
+      que eu possa visualizar seus status.
+    </h1>
+  );
 }
 export default Home;
