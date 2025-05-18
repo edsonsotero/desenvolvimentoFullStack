8d9518ba7202595eb0600a3cd979b22279466735
sotero.developer <sotero.developer@gmail.com>
2025-05-18 14:05:57 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-return <h1>Milca, você precisa desbloquear o silenciador do whatsApp para que eu possa visualizar seus status. 
+return <h1>Milca, bom dia! Você precisa desbloquear o silenciador do whatsApp para que eu possa visualizar seus status. 
 </h1>
 }
 export default Home;
