4f354059689b3a7e4f89736145f01e10f8e2be3d
sotero.developer <sotero.developer@gmail.com>
2025-05-18 20:56:43 +0000

    alterando mesagem

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
   return (
     <h1>
-      Milca, bom dia! Você precisa desbloquear o silenciador do whatsApp para
-      que eu possa visualizar seus status.
+      Felicitações, você foi agraciada com esta linda criança ... recebendo um
+      dos maiores dom da vida: ser mãe... Parabéns ao pai também que foi
+      agraciado, aos avós, tio se tias e todos os familiares que receberam ess
+      benção no seio de toda a família...
     </h1>
   );
 }
